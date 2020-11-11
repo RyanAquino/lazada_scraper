09ed5341289afe9e73efdd8de462d5618b1f99b4
RyanAquino <ryan.aquino7110@gmail.com>
2020-11-11 11:26:10 +0800

    initial

---
+++ index.py
@@ -0,0 +1,83 @@
+"""
+Author: Ryan Aquino
+Description: Scrape lazada.com products
+"""
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def selenium_html_scraper(url) -> str:
+    """
+    Returns the html source code of the url
+
+    :param url: source url
+    :return: html page source code
+    """
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = True
+
+    chrome_driver = webdriver.Chrome(options=chrome_options)
+    chrome_driver.get(url)
+    html_source = chrome_driver.page_source
+
+    chrome_driver.quit()
+    return html_source
+
+
+def scrape_product_item(product) -> dict:
+    """
+    Scrape a product details
+
+    :param product:
+    :return: dictionary of product name and price
+    """
+    product = product.findChildren("a", recursive=True)[0]
+    product_url = product.get("href")
+
+    product_html_source = selenium_html_scraper(f"https:{product_url}")
+    soup = BeautifulSoup(product_html_source, "html.parser")
+
+    title = soup.find("div", {"class": "pdp-product-title"}).text
+    price = soup.find("div", {"class": "pdp-product-price"}).text
+    price = f"₱{price.split('₱')[1]}"
+
+    product_details = {"name": title, "price": price}
+
+    return product_details
+
+
+def save_to_db(item):
+    """
+    Save product to database
+
+    :param item: product item
+    :return: None
+    """
+    pass
+
+
+def category_scraper(html):
+    """
+    Extracts the items in the html source of a product category
+
+    :param html: html page source code
+    :return: None
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    products = soup.find_all(attrs={"data-qa-locator": "product-item"})
+
+    for product in products:
+        product_details = scrape_product_item(product)
+        print(product_details)
+
+
+if __name__ == "__main__":
+    urls = [
+        "https://www.lazada.com.ph/shop-mobiles/",
+        "https://www.lazada.com.ph/shop-laptops/",
+        "https://www.lazada.com.ph/shop-desktop-computers/",
+    ]
+
+    for url in urls:
+        source = selenium_html_scraper(url)
+        category_scraper(source)
